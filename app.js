50051b536eddcd4ee6d2f3109cd084b4af5151e1
DorehidysMosquera <dorehidys@gmail.com>
2025-03-11 17:41:53 -0500

    Se creó la función agregar amigo

--- app.js
+++ app.js
@@ -3,4 +3,23 @@
 //Creación del array para almacenar los nombres de los amigos
 let listaDeAmigos = [];
 
+//Implementación de la función para agregar un nuevo nombre de amigo a el array listaDeAmigos
 
+function agregarAmigo(){
+    //Se conecta el elemento input que contendrá cada nombre de amigos a JavaScript a través del DOM almacenandolo en una variable objeto
+    let input = document.getElementById("amigo");
+
+    //Se almacena el texto del input en una variable llamada nombreAmigo
+    let nombreAmigo = input.value;
+
+    //Se valida que el campo de texto no esté vacío y se utiliza return para que se detenga la ejecución del código en caso de que el campo esté vacío. En caso de que no esté vacío se inserta el nombre capturado a la lista de amigos e inmediátamente se limpia el campo de entrada.
+    if(nombreAmigo === ""){
+        alert("Por favor, inserte un nombre");
+        return;
+    }else{
+        listaDeAmigos.push(nombreAmigo);
+        input.value = "";
+    }
+
+    //
+}console.log(listaDeAmigos);
